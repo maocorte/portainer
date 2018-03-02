2f04a878e2d8b6eeafecc35363a276220de360b7
Name From Git-Plugin-Test <mauro.cortellazzi@radicalbit.io>
2018-03-02 08:12:38 +0100

    Updated build file

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ ARCHIVE_BUILD_FOLDER="/tmp/portainer-builds"
 
 # parameter: "platform-architecture"
 function build_and_push_images() {
-  docker build -t "portainer/portainer:$1-${VERSION}" -f build/linux/Dockerfile .
-  docker tag  "portainer/portainer:$1-${VERSION}" "portainer/portainer:$1"
-  docker push "portainer/portainer:$1-${VERSION}"
-  docker push "portainer/portainer:$1"
+  docker build -t "maocorte/portainer:$1-${VERSION}" -f build/linux/Dockerfile .
+  docker tag  "maocorte/portainer:$1-${VERSION}" "maocorte/portainer:$1"
+  docker push "maocorte/portainer:$1-${VERSION}"
+  docker push "maocorte/portainer:$1"
 }
 
 # parameter: "platform-architecture"
